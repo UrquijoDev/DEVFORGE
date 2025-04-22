42f17322bed34727b2dcd36aabe5ca7a42560269
ClovenZoi <162339497+ClovenZoi@users.noreply.github.com>
2025-04-21 23:52:49 -0700

    Preview Coqueta de Panel Admin

--- DEVFORGE-TEST-4/Pages/Admin/Index.cshtml.cs
+++ DEVFORGE-TEST-4/Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,33 @@
-using Microsoft.AspNetCore.Mvc;
+using DEVFORGE_TEST_4.Models;
+using DEVFORGE_TEST_4.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace DEVFORGE_TEST_4.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Project> ProjectPreview { get; set; } = new();
+        public List<ServicePlan> ServicePreview { get; set; } = new();
+
         public void OnGet()
         {
+            ProjectPreview = _context.Projects
+                .OrderByDescending(p => p.Id)
+                .Take(3)
+                .ToList();
+
+            ServicePreview = _context.ServicePlan
+                .OrderByDescending(s => s.Id)
+                .Take(3)
+                .ToList();
         }
     }
 }
