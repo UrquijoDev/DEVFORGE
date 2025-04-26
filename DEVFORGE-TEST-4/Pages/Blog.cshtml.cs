47a636e8b06ebef64a89a01acf1abb5c0e4f9bd1
ClovenZoi <162339497+ClovenZoi@users.noreply.github.com>
2025-04-25 21:42:37 -0700

    Commit de cambios varios V2 xd

--- DEVFORGE-TEST-4/Pages/Blog.cshtml.cs
+++ DEVFORGE-TEST-4/Pages/Blog.cshtml.cs
@@ -1,5 +1,6 @@
 using DEVFORGE_TEST_4.Models;
 using DEVFORGE_TEST_4.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,23 +16,38 @@ namespace DEVFORGE_TEST_4.Pages
         }
 
         public List<UserProfile> UserProfiles { get; set; } = new();
-        public UserProfile SelectedUser { get; set; }
+        public UserProfile? SelectedUser { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? SelectedUserId { get; set; }
 
         public void OnGet()
         {
-            // Obtener todos los perfiles con la info básica
             UserProfiles = _context.UserProfiles
                 .Include(up => up.User)
                 .ToList();
 
-            // Seleccionar el primero y traer sus proyectos y etiquetas
-            SelectedUser = _context.UserProfiles
-                .Include(up => up.User)
-                .Include(up => up.Projects)
-                    .ThenInclude(up => up.Project)
-                        .ThenInclude(p => p.ProjectTags)
-                            .ThenInclude(pt => pt.Tag)
-                .FirstOrDefault();
+            if (SelectedUserId.HasValue)
+            {
+                SelectedUser = _context.UserProfiles
+                    .Include(up => up.User)
+                    .Include(up => up.Projects)
+                        .ThenInclude(up => up.Project)
+                            .ThenInclude(p => p.ProjectTags)
+                                .ThenInclude(pt => pt.Tag)
+                    .FirstOrDefault(up => up.Id == SelectedUserId.Value);
+            }
+
+            if (SelectedUser == null)
+            {
+                SelectedUser = _context.UserProfiles
+                    .Include(up => up.User)
+                    .Include(up => up.Projects)
+                        .ThenInclude(up => up.Project)
+                            .ThenInclude(p => p.ProjectTags)
+                                .ThenInclude(pt => pt.Tag)
+                    .FirstOrDefault();
+            }
         }
     }
 }
--- DEVFORGE-TEST-4/Pages/Index.cshtml.cs
+++ DEVFORGE-TEST-4/Pages/Index.cshtml.cs
@@ -1,20 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using DEVFORGE_TEST_4.Models;
+using Microsoft.EntityFrameworkCore;
+using DEVFORGE_TEST_4.Services;
 
 namespace DEVFORGE_TEST_4.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public List<UserProfile> TeamMembers { get; set; } = new();
 
+        public async Task OnGetAsync()
+        {
+            TeamMembers = await _context.UserProfiles
+                .Include(up => up.User)
+                .OrderBy(up => up.User.FirstName)
+                .ToListAsync();
         }
     }
 }
